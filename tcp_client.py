d817954ba254bee0ee460391328a376d97619e3d
boyqb2212 <95397242+boyqb2212@users.noreply.github.com>
2022-06-29 16:50:15 +0700

    Add files via upload

---
+++ tcp_client.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+ 
+import socket
+import logging
+logging.basicConfig(filename='example1.txt',level=logging.DEBUG)
+
+host="127.0.0.1"
+port = 9998
+
+try:
+	mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	mysocket.connect((host, port))
+	print('Connected to host '+str(host)+' in port: '+str(port))
+	message = mysocket.recv(1024)
+	print("Message received from the server", message)
+	while True:
+		message = input("Enter your message > ")
+		logging.info(  mysocket.send(bytes(message.encode('utf-8'))))
+		if message== "quit":
+			break
+except socket.errno as error:
+	print("Socket error ", error)
+finally:
+	mysocket.close()
+
